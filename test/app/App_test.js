b87705500c9fafd74535f98b287c802c7eed4ab8
Dylan Woods <dwoods@gogoair.com>
2016-05-14 21:16:52 -0600

    brunch-angular-material-skeleton
    - upgrading npm integration (migrating from 2 to 3)
    - optimizing test harness

--- test/app/App_test.js
+++ test/app/App_test.js
@@ -1,15 +1,15 @@
 describe('Module: myModule', function () {
   describe('Configuration', function () {
-    var controller, location, scope, state;
+    var $controller, $location, $rootScope, $state;
 
     beforeEach(module('myModule'));
 
     beforeEach(inject(
       function (_$controller_, _$location_, _$rootScope_, _$state_) {
-        controller = _$controller_;
-        location = _$location_;
-        scope = _$rootScope_;
-        state = _$state_;
+        $controller = _$controller_;
+        $location = _$location_;
+        $rootScope = _$rootScope_;
+        $state = _$state_;
       }
     ));
 
@@ -17,28 +17,44 @@ describe('Module: myModule', function () {
       $httpBackend.verifyNoOutstandingRequest();
     }));
 
-     it('redirects to `/` otherwise', function() {
-        location.path('/nonExistentPath');
-        scope.$emit('$locationChangeSuccess');
-        expect(location.path()).to.equal('/');
+    describe('route: `/`', function () {
+      var route;
+
+      beforeEach(function () {
+        route = $state.get('route');
+      });
+
+      it('should resolve url to `/`', function () {
+        expect(route.url).to.equal('/');
+      });
+
+      it('renders the status page', function () {
+        expect(route.templateUrl).to.equal('pages/route.html');
+      });
+
+      it('uses the right controller', function () {
+        expect(route.controller).to.equal('RouteController');
+      });
     });
 
-    describe('Routng States', function () {
-      describe('home: `/`', function () {
-        var home;
+    describe('notFound: `any path not defined in $stateProvider`', function () {
+      var notFound;
 
-        beforeEach(function () {
-          home = state.get('home');
-        });
+      beforeEach(function () {
+        notFound = $state.get('notFound');
+      });
 
-        it('should resolve url to `/`', function () {
-          expect(home.url).to.equal('/');
-        });
+      it('should resolve url to `/`', function () {
+        expect(notFound.url).to.equal('^*path');
+      });
+
+      it('renders the status page', function () {
+        expect(notFound.templateUrl).to.equal('404.html');
       });
     });
 
     it('should set $locationProvider to html5Mode', function () {
-      expect(location.$$html5).to.be.true;
+      expect($location.$$html5).to.be.true;
     });
   });
 });
